e7f4decde907f4a8594f375acf43a6b0672fa44a
Alcarian <floa2@msn.com>
2023-10-20 16:02:32 +0200

    maj CV20-10-23

--- src/components/DownloadCV.js
+++ src/components/DownloadCV.js
@@ -5,9 +5,13 @@ import * as docx from "docx";
 export default function DownloadCV() {
   return (
     <div className="downloadCV">
-      <img src="./img/CV.PNG" alt="" />
+      <img src="./img/CV20-10-23.PNG" alt="" />
 
-      <a href="./CV/monCV.docx" download="monCV.docx" className="hover">
+      <a
+        href="./CV/CV DEV 20-10-23.docx"
+        download="CV DEV 20-10-23.docx"
+        className="hover"
+      >
         <span className="button">Télécharger mon CV</span>
       </a>
     </div>
